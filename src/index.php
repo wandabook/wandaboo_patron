1824cb59fa62e6007664f6affed569a50ed3803a
Your Name <styvesdaudet@gmail.com>
2024-11-01 14:49:45 +0100

    Test parameter

--- src/index.php
+++ src/index.php
@@ -15,6 +15,16 @@ return function ($context) {
         ->setProject(getenv('APPWRITE_FUNCTION_PROJECT_ID'))
         ->setKey($context->req->headers['x-appwrite-key']);
     $users = new Users($client);
+    $database = new Databases($client);
+    // try to display all the infomation data return the parameter
+    $context->log($context->req->scheme);               // Value of the x-forwarded-proto header, usually http or https
+    $context->log($context->req->method);               // Request method, such as GET, POST, PUT, DELETE, PATCH, etc.
+    $context->log($context->req->url);                  // Full URL, for example: http://awesome.appwrite.io:8000/v1/hooks?limit=12&offset=50
+    $context->log($context->req->host);                 // Hostname from the host header, such as awesome.appwrite.io
+    $context->log($context->req->port);                 // Port from the host header, for example 8000
+    $context->log($context->req->path);                 // Path part of URL, for example /v1/hooks
+    $context->log($context->req->queryString);          // Raw query params string. For example "limit=12&offset=50"
+    // The req object contains the request data
     if($context->req->method ==='POST'){
         if ($context->req->path === '/patron') {
             try {
