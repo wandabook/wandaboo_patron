37e6bf6e835a4755bf9a02d3c073cf2a3c33fc75
Your Name <styvesdaudet@gmail.com>
2024-11-01 13:43:15 +0100

    inint

--- src/index.php
+++ src/index.php
@@ -1,7 +1,6 @@
 <?php
-
 require_once(__DIR__ . '/../vendor/autoload.php');
-
+require_once("api.php");
 use Appwrite\Client;
 use Appwrite\Services\Users;
 
@@ -24,14 +23,30 @@ return function ($context) {
     } catch(Throwable $error) {
         $context->error('Could not list users: ' . $error->getMessage());
     }
-
+       
     // The req object contains the request data
-    if ($context->req->path === '/ping') {
-        // Use res object to respond with text(), json(), or binary()
-        // Don't forget to return a response!
-        return $context->res->text('Pong');
+    if ($context->req->path === '/patron') {
+ try {
+        $params = array(
+        'first_name' => 'Mary',
+        'last_name' => 'Shelley',
+        'email' => 'frankenstein@example.com',
+        'notification_email' => 'frankenstein@example.com,another.email@example.com',
+        'password' => '2ab3940as94ikd2394k'
+        );      
+        // Convertir le tableau PHP en JSON
+        $json_data = json_encode($params);
+    
+        $api = new API("https://api.libib.com");
+        $response = $api->post('/patrons',$params,getenv('APPWRITE_API_KEY'),getenv('APPWRITE_API_USER') );
+        
+      return $context->res->json([
+        'motto' =>$response,
+        ]);
+    }catch(Throwable $error) {
+        $context->error('Could not list users: ' . $error->getMessage());
     }
-
+    }
     return $context->res->json([
         'motto' => 'Build like a team of hundreds_',
         'learn' => 'https://appwrite.io/docs',
