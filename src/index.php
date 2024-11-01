5d9119cf216cb9a0b3fe2f5b87ac605753575c06
Your Name <styvesdaudet@gmail.com>
2024-11-01 14:32:25 +0100

    Test parameter

--- src/index.php
+++ src/index.php
@@ -14,41 +14,7 @@ return function ($context) {
         ->setProject(getenv('APPWRITE_FUNCTION_PROJECT_ID'))
         ->setKey($context->req->headers['x-appwrite-key']);
     $users = new Users($client);
-    // try to display all the infomation data return the parameter
-    $context->log(json_encode($context->req->bodyJson));// Object from parsed JSON request body, otherwise string
-    $context->log(json_encode($context->req->headers)); // String key-value pairs of all request headers, keys are lowercase
-    $context->log($context->req->scheme);               // Value of the x-forwarded-proto header, usually http or https
-    $context->log($context->req->method);               // Request method, such as GET, POST, PUT, DELETE, PATCH, etc.
-    $context->log($context->req->url);                  // Full URL, for example: http://awesome.appwrite.io:8000/v1/hooks?limit=12&offset=50
-    $context->log($context->req->host);                 // Hostname from the host header, such as awesome.appwrite.io
-    $context->log($context->req->port);                 // Port from the host header, for example 8000
-    $context->log($context->req->path);                 // Path part of URL, for example /v1/hooks
-    $context->log($context->req->queryString);          // Raw query params string. For example "limit=12&offset=50"
-    $context->log(json_encode($context->req->query))
-    // The req object contains the request data
-    if($context->req->method ==='POST'){
-        if ($context->req->path === '/patron') {
-            try {
-                $params = array(
-                'first_name' => 'Mary',
-                'last_name' => 'Shelley',
-                'email' => 'frankenstein@example.com',
-                'notification_email' => 'frankenstein@example.com,another.email@example.com',
-                'password' => '2ab3940as94ikd2394k'
-                );      
-                // Convertir le tableau PHP en JSON
-                $json_data = json_encode($context->req->bodyJson);
-            
-                $api = new API("https://api.libib.com");
-                $response = $api->post('/patrons',$params,getenv('APPWRITE_API_KEY'),getenv('APPWRITE_API_USER') );
-                    
-                return $context->res->json(['motto' =>$response,]);
-            }catch(Throwable $error) {
-                $context->error('Could not list users: ' . $error->getMessage());
-            }
-        }
-        
-    }
+
     return $context->res->json([
         'motto' => 'Build like a team of hundreds_',
         'learn' => 'https://appwrite.io/docs',
