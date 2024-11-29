16f43d028ca5fa9cbe3976340b77460342cf1aba
Your Name <styvesdaudet@gmail.com>
2024-11-29 10:38:44 +0100

    delete user

--- src/index.php
+++ src/index.php
@@ -27,7 +27,9 @@ return function ($context) {
                     'last_name' => $json_data['last_name'],
                     'email' => $json_data['email'],
                     'notification_email' => $json_data['notification_email'],
-                    'password' => $json_data['password']
+                    'password' => $json_data['password'],
+                    'patron_id' => $json_data['patron_id'],
+                    'tags'=>$json_data['tags'],
                 );
                
                 $api = new API("https://api.libib.com");
@@ -56,8 +58,8 @@ return function ($context) {
             }
         }
         
-    }else
-    if($context->req->method ==='DELETE'){
+    }*/
+    else if($context->req->method ==='DELETE'){
         if ($context->req->path === '/patron') {
             try {
                 $context->log("".$context->req->bodyJson);
@@ -67,12 +69,12 @@ return function ($context) {
                 $response = $api->delete("/patrons".'/'.$json_data['patron'],null,getenv('APPWRITE_API_KEY'),getenv('APPWRITE_API_USER') );
                 return $context->res->json(['result' =>$response,]);
             }catch(Throwable $error) {
-                $context->error('Could not list users: ' . $error->getMessage() .'Error: ');
+                $context->error('Could not delete users: ' . $error->getMessage() .'Error: ');
                 $context->error('Line: ' . $error->getLine() .'Error: ');
             }
         }
         
-    }*/
+    }
     return $context->res->json([
         'motto' => 'Build like a team of hundreds_',
         'learn' => 'https://appwrite.io/docs',
