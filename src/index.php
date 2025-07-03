2bfaaec7035f1891eb3f72dae0e8ec8bfba45348
Appwrite <team@appwrite.io>
2025-07-03 09:48:50 +0000

    Create 'Libib patron' function

--- src/index.php
+++ src/index.php
@@ -1,9 +1,9 @@
 <?php
+
 require_once(__DIR__ . '/../vendor/autoload.php');
-require_once("api.php");
+
 use Appwrite\Client;
 use Appwrite\Services\Users;
-use Appwrite\Services\Databases;
 
 // This Appwrite function will be executed every time your function is triggered
 return function ($context) {
@@ -15,92 +15,23 @@ return function ($context) {
         ->setProject(getenv('APPWRITE_FUNCTION_PROJECT_ID'))
         ->setKey($context->req->headers['x-appwrite-key']);
     $users = new Users($client);
+
+    try {
+        $response = $users->list();
+        // Log messages and errors to the Appwrite Console
+        // These logs won't be seen by your end users
+        $context->log('Total users: ' . $response['total']);
+    } catch(Throwable $error) {
+        $context->error('Could not list users: ' . $error->getMessage());
+    }
+
     // The req object contains the request data
-    if($context->req->method ==='POST'){
-        if ($context->req->path === '/patron') {
-            try {
-                $context->log("".$context->req->bodyJson);
-                // Convertir le tableau PHP en JSON
-                $json_data = $context->req->bodyJson;
-                $params = array(
-                    'first_name' =>  $json_data['first_name'],
-                    'last_name' => $json_data['last_name'],
-                    'email' => $json_data['email'],
-                    'notification_emails' => $json_data['notification_email'],
-                    'password' => $json_data['password'],
-                    'patron_id' => $json_data['patron_id'],
-                    'tags'=>$json_data['tags'],
-                );
-               
-                $api = new API("https://api.libib.com");
-                $response = $api->post('/patrons',$params,getenv('APPWRITE_API_KEY'),getenv('APPWRITE_API_USER') );
-                return $context->res->json(['result' =>$response,]);
-            }catch(Throwable $error) {
-                $context->error('Could not list users: ' . $error->getMessage() .'Error: ');
-                $context->error('Line: ' . $error->getLine() .'Error: ');
-            }
-        }
-        
+    if ($context->req->path === '/ping') {
+        // Use res object to respond with text(), json(), or binary()
+        // Don't forget to return a response!
+        return $context->res->text('Pong');
     }
-    else if($context->req->method ==='GET'){
-        if ($context->req->path === '/patron') {
-            try {
-                $context->log("".$context->req->bodyJson);
-                // Convertir le tableau PHP en JSON
-                $json_data = $context->req->bodyJson;
-                $api = new API("https://api.libib.com");
-                $response = $api->get("/patrons".'/'.$json_data['patron'],null,getenv('APPWRITE_API_KEY'),getenv('APPWRITE_API_USER') );
-                return $context->res->json(['result' =>$response,]);
-            }catch(Throwable $error) {
-                $context->error('Could not list users: ' . $error->getMessage() .'Error: ');
-                $context->error('Line: ' . $error->getLine() .'Error: ');
-            }
-        }
-        
-    }
-    else if($context->req->method ==='DELETE'){
-        if ($context->req->path === '/patron') {
-            try {
-                $context->log("".$context->req->bodyJson);
-                // Convertir le tableau PHP en JSON
-                $json_data = $context->req->bodyJson;
-                $api = new API("https://api.libib.com");
-                $response = $api->delete("/patrons".'/'.$json_data['barcode'],getenv('APPWRITE_API_KEY'),getenv('APPWRITE_API_USER') );
-                return $context->res->json(['result' =>$response,]);
-            }catch(Throwable $error) {
-                $context->error('Could not delete users: ' . $error->getMessage() .'Error: ');
-                $context->error('Line: ' . $error->getLine() .'Error: ');
-                return $context->res->json(['error' =>$error->getMessage(),]);
-            }
-        }
-        
-    }else if($context->req->method ==='PUT'){
-        if ($context->req->path === '/patron') {
-            try {
-                $context->log("".$context->req->bodyJson);
-                // Convertir le tableau PHP en JSON
-                $json_data = $context->req->bodyJson;
-                $params = array(
-                    //'first_name' =>  $json_data['first_name'],
-                    //'last_name' => $json_data['last_name'],
-                    //'email' => $json_data['email'],
-                    //'notification_email' => $json_data['notification_email'],
-                    //'password' => $json_data['password'],
-                    'patron_id' => $json_data['patron_id'],
-                    'tags'=>$json_data['tags'],
-                    'freeze'=>$json_data['freeze'],
-                );
-               
-                $api = new API("https://api.libib.com");
-                $response = $api->post('/patrons'.'/'.$json_data['barcode'],$params,getenv('APPWRITE_API_KEY'),getenv('APPWRITE_API_USER') );
-                return $context->res->json(['result' =>$response,]);
-            }catch(Throwable $error) {
-                $context->error('Update user' . $error->getMessage() .'Error: ');
-                $context->error('Line: ' . $error->getLine() .'Error: ');
-            }
-        }
-        
-    }
+
     return $context->res->json([
         'motto' => 'Build like a team of hundreds_',
         'learn' => 'https://appwrite.io/docs',
